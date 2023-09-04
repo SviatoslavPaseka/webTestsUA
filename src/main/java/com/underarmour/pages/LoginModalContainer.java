b3db6439e394275a0ea2fda2131a4857e4cfe788
SviatoslavPaseka <paseka.syat@gmail.com>
2023-09-04 17:18:31 +0300

    new pipeline 4

--- src/main/java/com/underarmour/pages/LoginModalContainer.java
+++ src/main/java/com/underarmour/pages/LoginModalContainer.java
@@ -1,12 +1,16 @@
 package com.underarmour.pages;
 
-import com.zebrunner.carina.utils.factory.DeviceType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 public class LoginModalContainer extends AbstractPage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public LoginModalContainer(WebDriver driver) {
         super(driver);
     }
@@ -28,19 +32,23 @@ public class LoginModalContainer extends AbstractPage {
     }
 
     public void inputEmail(String email){
+        LOGGER.info("inputting email");
         emailField.type(email);
     }
 
     public void inputPassword(String password){
+        LOGGER.info("inputting password");
         passwordField.type(password);
     }
 
     public void inputCredentials(String email, String password){
+        LOGGER.info("inputting creds");
         inputEmail(email);
         inputPassword(password);
     }
 
     public void clickLoginButton(){
+        LOGGER.info("click login button");
         loginButton.click();
     }
 }
